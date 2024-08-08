de352744125479eb8b021d15f0129c73779c90d5
Georg Roll <georg.roll@gmail.com>
2024-08-08 20:07:12 +0200

    assignment6p1 implemented

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -16,6 +16,9 @@
 #include <signal.h>
 #include <syslog.h>
 #include <stdbool.h>
+#include <pthread.h>
+#include <time.h>
+#include "queue.h"
 
 #define PORT "9000" // the port users will be connecting to
 
@@ -23,12 +26,22 @@
 #define BUFFERSIZE 500000
 size_t bufferSize = BUFFERSIZE;
 
-char *socketbuf;
-char *filereadbuf;
+// SLIST.
+typedef struct slist_data_s slist_data_t;
+struct slist_data_s
+{
+    pthread_t thread_id;
+    bool done;
+    int connection_fd;
+    SLIST_ENTRY(slist_data_s)
+    entries;
+};
 
 int sockfd, new_fd; // listen on sock_fd, new connection on new_fd
 FILE *file;
 char datadir[] = "/var/tmp/aesdsocketdata";
+
+pthread_mutex_t counter_mutex;
 
 void sigchld_handler(int s)
 {
@@ -81,6 +94,109 @@ void *get_in_addr(struct sockaddr *sa)
     return &(((struct sockaddr_in6 *)sa)->sin6_addr);
 }
 
+void *thread_connection(void *arg)
+{
+    slist_data_t *this_datap = (slist_data_t *)arg;
+
+    char *socketbuf = (char *)malloc(bufferSize + sizeof(char));
+    char *filereadbuf = (char *)malloc(bufferSize + sizeof(char));
+
+    if ((socketbuf == NULL) || (filereadbuf == NULL))
+    {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(EXIT_FAILURE); // Exit with error code
+    }
+
+    int bytes_received;
+    int bytes_read;
+
+    while ((bytes_received = recv(this_datap->connection_fd, socketbuf, BUFFERSIZE - 1, 0)) > 0)
+    {
+
+        socketbuf[bytes_received] = '\0'; // Null-terminate the received data
+        // printf("Received: %s\n", socketbuf);
+
+        /* code */
+        syslog(LOG_DEBUG, "Writing %s to %s", socketbuf, datadir);
+        printf("thread writing file: %s", socketbuf);
+        fprintf(file, "%s", socketbuf);
+        fflush(file);
+
+        if (socketbuf[bytes_received - 1] == '\n')
+        {
+
+            // Lock the mutex before accessing the shared resource
+            pthread_mutex_lock(&counter_mutex);
+
+            // Critical section
+
+            rewind(file);
+            while ((bytes_read = fread(filereadbuf, 1, BUFFERSIZE, file)) > 0)
+            {
+                syslog(LOG_DEBUG, "read: %i", bytes_read);
+                printf("thread Bytes read: %i\n", bytes_read);
+
+                int sent;
+                if ((sent = send(this_datap->connection_fd, filereadbuf, bytes_read, 0)) == -1)
+                {
+                    perror("Send failed");
+                    close(this_datap->connection_fd);
+                    fclose(file);
+                    pthread_mutex_unlock(&counter_mutex);
+                    exit(EXIT_FAILURE);
+                }
+                else
+                {
+                    syslog(LOG_DEBUG, "sent: %i", sent);
+                    printf("thread Packet end bytes sent: %i\r\n", sent);
+                }
+            }
+            // Unlock the mutex after accessing the shared resource
+            pthread_mutex_unlock(&counter_mutex);
+            break;
+        }
+    }
+    printf("closed with: %i", close(this_datap->connection_fd));
+    //  exit(EXIT_FAILURE);
+
+    free(socketbuf);
+    free(filereadbuf);
+    // syslog(LOG_DEBUG, "Closed connection from %s", s);
+    close(this_datap->connection_fd);
+    this_datap->done = true;
+    // exit(EXIT_SUCCESS);
+    return NULL;
+}
+
+void *thread_timer(void *arg)
+{
+
+    char buffer[100];
+    time_t current_time;
+    struct tm *time_info;
+
+    while (1)
+    {
+        // Get the current time
+        time(&current_time);
+        time_info = localtime(&current_time);
+
+        // Format the time according to RFC 2822 compliant strftime format
+        // Example format: "timestamp: Fri, 21 Nov 1997 09:55:06 +0000\n"
+        strftime(buffer, sizeof(buffer), "timestamp: %a, %d %b %Y %H:%M:%S %z\n", time_info);
+        pthread_mutex_lock(&counter_mutex);
+
+        // Write the formatted timestamp to the file
+        fputs(buffer, file);
+        printf("timerwrite: %s", buffer);
+        pthread_mutex_unlock(&counter_mutex);
+
+        // Sleep for 10 seconds
+        sleep(10);
+    }
+
+    return 0;
+}
 int main(int argc, char *argv[])
 {
     openlog("aesdsocket", 0, LOG_USER);
@@ -92,6 +208,19 @@ int main(int argc, char *argv[])
     int yes = 1;
     char s[INET6_ADDRSTRLEN];
     int rv;
+
+    // Slist
+    slist_data_t *datap = NULL;
+
+    SLIST_HEAD(slisthead, slist_data_s)
+    head;
+    SLIST_INIT(&head);
+
+    if (pthread_mutex_init(&counter_mutex, NULL) != 0)
+    {
+        fprintf(stderr, "Mutex initialization failed\n");
+        return 1;
+    }
 
     // Setup signal handling
     signal(SIGINT, handle_signal);
@@ -145,13 +274,21 @@ int main(int argc, char *argv[])
     bool daemon = false;
     if (argc == 2 && (strcmp(argv[1], "-d") == 0))
     {
-        printf("daeeeeeemon!");
+        // printf("daeeeeeemon!");
         daemon = true;
     }
 
     if (!daemon || !fork())
     { // child
         // close(sockfd);
+        pthread_t timer_thread;
+        if (pthread_create(&timer_thread, NULL, thread_timer, NULL))
+        {
+            printf("timer Thread failed.");
+            syslog(LOG_ERR, "timer Thread failed.");
+            closelog();
+            exit(1);
+        }
 
         if (listen(sockfd, BACKLOG) == -1)
         {
@@ -168,7 +305,7 @@ int main(int argc, char *argv[])
             exit(1);
         }
 
-        printf("server: waiting for connections...\n");
+        printf("main server: waiting for connections...\n");
 
         file = fopen(datadir, "w+");
         if (file == NULL)
@@ -182,6 +319,7 @@ int main(int argc, char *argv[])
         while (1)
         { // main accept() loop
             sin_size = sizeof their_addr;
+            printf("main wait for new accept");
             new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
             if (new_fd == -1)
             {
@@ -192,64 +330,40 @@ int main(int argc, char *argv[])
             inet_ntop(their_addr.ss_family,
                       get_in_addr((struct sockaddr *)&their_addr),
                       s, sizeof s);
-            syslog(LOG_DEBUG, "Accepted connection from %s", s);
 
+            syslog(LOG_DEBUG, "Accepted connection from %s", s);
             printf("server: got connection from %s\n", s);
 
-            socketbuf = (char *)malloc(bufferSize + sizeof(char));
-            filereadbuf = (char *)malloc(bufferSize + sizeof(char));
+            pthread_t thread;
+            datap = malloc(sizeof(slist_data_t));
+            datap->done = false;
+            datap->connection_fd = new_fd;
 
-            if ((socketbuf == NULL) || (filereadbuf == NULL))
+            if (pthread_create(&thread, NULL, thread_connection, datap))
             {
-                fprintf(stderr, "Memory allocation failed\n");
-                return 1; // Exit with error code
+                printf("Thread failed.");
+                syslog(LOG_ERR, "Thread failed.");
+                closelog();
+                exit(1);
             }
+            datap->thread_id = thread;
 
-            int bytes_received;
-            int bytes_read;
+            printf("newthread: %li\n", datap->thread_id);
+            SLIST_INSERT_HEAD(&head, datap, entries);
 
-            while ((bytes_received = recv(new_fd, socketbuf, BUFFERSIZE - 1, 0)) > 0)
+            // Read1.
+            printf("Threads: ");
+            SLIST_FOREACH(datap, &head, entries)
             {
-
-                socketbuf[bytes_received] = '\0'; // Null-terminate the received data
-                // printf("Received: %s\n", socketbuf);
-
-                /* code */
-                syslog(LOG_DEBUG, "Writing %s to %s", socketbuf, datadir);
-                fprintf(file, "%s", socketbuf);
-                fflush(file);
-
-                if (socketbuf[bytes_received - 1] == '\n')
+                printf("%ld, ", datap->thread_id);
+                if (datap->done)
                 {
-                    rewind(file);
-                    while ((bytes_read = fread(filereadbuf, 1, BUFFERSIZE, file)) > 0)
-                    {
-                        syslog(LOG_DEBUG, "read: %i", bytes_read);
-                        printf("Bytes read: %i", bytes_read);
-
-                        int sent;
-                        if ((sent = send(new_fd, filereadbuf, bytes_read, 0)) == -1)
-                        {
-                            perror("Send failed");
-                            close(new_fd);
-                            fclose(file);
-                            exit(EXIT_FAILURE);
-                        }
-                        else
-                        {
-                            syslog(LOG_DEBUG, "sent: %i", sent);
-                            printf("Packetend bytes sent: %i, pos: \r\n", sent);
-                        }
-                    }
-                    break;
+                    pthread_join(datap->thread_id, NULL);
                 }
             }
-
-            free(socketbuf);
-            free(filereadbuf);
-            syslog(LOG_DEBUG, "Closed connection from %s", s);
-            close(new_fd);
+            printf("\n");
         }
+
         fclose(file);
     } // end child
 
